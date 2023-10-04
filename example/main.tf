194212b10474a515ef5d7269ee651bb22cc0c56c
Ryan Lafferty <1731734+lafferrs@users.noreply.github.com>
2023-10-04 16:11:37 -0400

    main

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,5 @@
 module "ami-tracker" {
-  source = "git::https://github.com/Evernorth/aws-ami-tracker.git//terraform?ref=initial"
+  source = "git::https://github.com/Evernorth/aws-ami-tracker.git//terraform?ref=main"
 
   app_name         = "AmiTrackerExample"
   environment      = "dev"
